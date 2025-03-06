560e444e0ed4a84d0854548d340daf9c97e0cb02
0xHexecutioner <116597049+0xHexecutioner@users.noreply.github.com>
2025-03-06 18:33:14 +0100

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int appleCost = 2;
+		int buyer = 6;
+		int totalCost;
+		totalCost = 2 * 6;
+		System.out.println(totalCost + " $");
+		
+	
+	
+	}
+}
